31b65d751affab4fcec34625920b3d687972c84e
Pamaq <paulinakuta00@gmail.com>
2022-10-10 21:39:55 +0200

    Modified resizing functions

--- script.js
+++ script.js
@@ -2,22 +2,34 @@ const plusBtn = document.querySelector(".sizeUp");
 const minusBtn = document.querySelector(".sizeDown");
 const colorBtn = document.querySelector(".color");
 const textPar = document.querySelector("p");
+let fontSize = 36;
 //przycisk plus ma powiekszac tekst
 //przycisk minus ma pomniejszac tekst
 //przycisk color ma zmieniac kolor tekstu
 const plus = () => {
-	textPar.classList.toggle("bigger");
+	if (fontSize > 81) return;
+	fontSize += 5;
+	textPar.style.fontSize = fontSize + "px";
 };
 
 const minus = () => {
-	textPar.classList.toggle("smaller");
+	if (fontSize < 21) return;
+	fontSize -= 5;
+	textPar.style.fontSize = fontSize + "px";
 };
 
-let colors = ["red", "blue", "pink", "purple", "green", "yellow"];
+// let colors = ["red", "blue", "pink", "purple", "green", "yellow"];
 
 const color = () => {
-	let number = Math.floor(Math.random() * colors.length);
-	textPar.style.color = colors[number];
+	// let number = Math.floor(Math.random() * colors.length);
+	// textPar.style.color = colors[number]; sposob I czyli moj
+	const r = Math.floor(Math.random() * 255);
+	console.log(r);
+	const g = Math.floor(Math.random() * 255);
+	console.log(g);
+	const b = Math.floor(Math.random() * 255);
+	console.log(b);
+	textPar.style.color = rgb("r", "g", "b");
 };
 
 plusBtn.addEventListener("click", plus);
